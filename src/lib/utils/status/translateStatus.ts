eb459fa27de49e2f71dc3772fd023080bbb57c80
DinhHoa14 <hoamndse173090@fpt.edu.vn>
2025-07-26 00:11:32 +0700

    Fix/appointment status

--- src/lib/utils/status/translateStatus.ts
+++ src/lib/utils/status/translateStatus.ts
@@ -4,14 +4,8 @@ export const translateStatus = (status: AppointmentStatus): string => {
   switch (status) {
     case "PENDING":
       return "Đang chờ";
-    case "CHECKIN":
-      return "Có mặt";
     case "PAID":
       return "Đã thanh toán";
-    case "PROCESS":
-      return "Đang khám";
-    case "CONFIRMED":
-      return "Đã xác nhận";
     case "COMPLETED":
       return "Hoàn tất";
     case "CANCELLED":
